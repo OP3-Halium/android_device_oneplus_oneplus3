8e162098153e2dacd63f1e7bc9d9b779e3529ce6
Simon Shi <simonsmh@gmail.com>
2018-09-02 19:10:48 +0800

    op3: Build light.* with BOARD_VNDK_VERSION
    Test: m -j BOARD_VNDK_VERSION=current lights.msm8996
    Change-Id: Iaa662222d6021b87e62182a6f9d1476c0c06c98a

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-
+LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_MODULE := lights.msm8996
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
